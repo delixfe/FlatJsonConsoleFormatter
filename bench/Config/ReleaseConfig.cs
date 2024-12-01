f464273c728127d56014706823bf190fd430c942
Deierlein Felix BIT <felix.deierlein@bit.admin.ch>
2024-12-01 14:06:10 +0100

    bench: set runtime to .NET8 and additionally .NET9 guarded by flag BENCH_9
    .NET9 is faster, allocates less or on GEN0.

--- bench/Config/ReleaseConfig.cs
+++ bench/Config/ReleaseConfig.cs
@@ -1,6 +1,7 @@
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Exporters.Json;
 using BenchmarkDotNet.Filters;
@@ -15,15 +16,18 @@ public class ReleaseConfig : ManualConfig
     public ReleaseConfig()
     {
         AddDefaults();
-        AddJob(Job.Default
-                .WithMinIterationCount(15)
-                .WithMaxIterationCount(100) // we don't want to run more than 100 iterations
-                .DontEnforcePowerPlan() // make sure BDN does not try to enforce the High Performance power plan on Windows
-                .WithGcServer(true)
-                .WithUnrollFactor(
-                    1024 * 8) // we need to reach 100 ms of execution time; unrolling is the easiest way to do it
-        );
+        var defaultJob = Job.Default
+            .WithMinIterationCount(15)
+            .WithMaxIterationCount(100) // we don't want to run more than 100 iterations
+            .DontEnforcePowerPlan() // make sure BDN does not try to enforce the High Performance power plan on Windows
+            .WithGcServer(true)
+            .WithUnrollFactor(
+                1024 * 8); // we need to reach 100 ms of execution time; unrolling is the easiest way to do it
 
+        AddJob(defaultJob.WithRuntime(CoreRuntime.Core80));
+#if BENCH_NET9
+        AddJob(defaultJob.WithRuntime(CoreRuntime.Core90));
+#endif
         AddColumn(StatisticColumn.Median, StatisticColumn.Min, StatisticColumn.Max,
             CategoriesColumn.Default);
 
